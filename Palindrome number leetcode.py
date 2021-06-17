9481c9f53d51cddfa28c284466be78a3ead88497
rahul-madaan <madan.rahul9@gmail.com>
2021-06-17 10:43:13 +0530

    solution by converting to reverse number

--- Palindrome number leetcode.py
+++ Palindrome number leetcode.py
@@ -1,19 +1,33 @@
 
 def isPalindrome(x: int) -> bool:
+    # if (x < 0):
+    #     return bool(0)
+    # length = len(str(x))
+    #
+    # listOfDigits = list(str(x))
+    # print(listOfDigits)
+    # for x in range(length // 2):
+    #     if listOfDigits[x] != listOfDigits[length - x-1]:
+    #         return bool(0)
+    #
+    # return bool(1)
+
     if (x < 0):
         return bool(0)
-    length = len(str(x))
 
-    listOfDigits = list(str(x))
-    print(listOfDigits)
-    for x in range(length // 2):
-        if listOfDigits[x] != listOfDigits[length - x-1]:
-            return bool(0)
+    copy = x
+    rev = 0
+    while copy != 0:
+        temp = copy % 10
+        rev = rev * 10
+        rev = rev + temp
+        copy = copy // 10
 
-    return bool(1)
-
-
-
+    print(rev)
+    if rev == x:
+        return bool(1)
+    else:
+        return bool(0)
 
 
 print(isPalindrome(123456654321))
