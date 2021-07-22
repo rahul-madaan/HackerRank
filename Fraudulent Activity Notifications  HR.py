2e94549104f3e94aa79d0d1be97866977b3b6808
rahul-madaan <madan.rahul9@gmail.com>
2021-07-22 16:57:08 +0530

    using selection sort

--- Fraudulent Activity Notifications  HR.py
+++ Fraudulent Activity Notifications  HR.py
@@ -1,46 +1,31 @@
-def partition(arr,low,high):
-   i = ( low-1 )
-   pivot = arr[high] # pivot element
-   for j in range(low , high):
-      # If current element is smaller
-      if arr[j] <= pivot:
-         # increment
-         i = i+1
-         arr[i],arr[j] = arr[j],arr[i]
-   arr[i+1],arr[high] = arr[high],arr[i+1]
-   return ( i+1 )
-# sort
-def quickSort(arr,low,high):
-   if low < high:
-      # index
-      pi = partition(arr,low,high)
-      # sort the partitions
-      quickSort(arr, low, pi-1)
-      quickSort(arr, pi+1, high)
-# main
-
-
-
 def activityNotifications(expenditure, d):
-    noti = 0
-    for i in range(len(expenditure) - d):
-        print(i)
-        focus_expenditure = expenditure[i:i+d]
-        print(focus_expenditure)
-        quickSort(focus_expenditure, 0, d-1)
-        mid = 0
-        if d % 2 == 1:
-            mid = focus_expenditure[(d // 2)]
+    freq = {}
+    notify=0
+    def find(idx):
+        total_count = 0
+        for i in range(201):
+            if i in freq:
+                total_count = total_count + freq[i]
+            if total_count >= idx:
+                return i
+    for i in range(len(expenditure)-1):
+        if expenditure[i] in freq:
+            freq[expenditure[i]]+=1
         else:
-            mid = (focus_expenditure[(d // 2)-1] + focus_expenditure[(d // 2)]) / 2
+            freq[expenditure[i]]=1
+        #print(f"i: {i},val: {expenditure[i]}, freq: {freq}")
+        if i>=d-1:
+            if d%2 ==0:
+                median = (find(d//2)+find(d//2+1))/2
+            else:
+                median = find(d/2)
+            #print("median: ",median)
+            if expenditure[i+1]>= (median*2) :
+                notify +=1
+                print("notify: ",notify)
+            #remove the previous element from dictionary
+            freq[expenditure[i-d+1]]-=1
 
-        # print(expenditure[d+i])
-        if expenditure[d + i] >= mid * 2:
-            noti = noti + 1
-
-    print(noti)
-
-    return noti
-
+    return notify
 
 activityNotifications([10,20,30,40,50], 4)
